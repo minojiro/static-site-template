7af5fc640320415767cc26530c5ff2eac3bd8238
minojiro <minojiro@users.noreply.github.com>
2021-03-18 17:09:20 +0900

    add: pug

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
 const sass =  require('sass')
 const DIST_PATH = path.join(__dirname, 'dist')
@@ -10,7 +10,7 @@ const DIST_PATH = path.join(__dirname, 'dist')
 const htmlPlugins = [
   new HtmlWebpackPlugin({
     filename: 'index.html',
-    template: 'src/templates/home.ejs',
+    template: 'src/templates/home.pug',
   }),
 ]
 
@@ -25,6 +25,18 @@ module.exports = (_, { mode }) => {
     },
     module: {
       rules: [
+        {
+          test: /\.pug$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'pug-loader',
+              options: {
+                pretty: true,
+              },
+            },
+          ],
+        },
         {
           test: /\.ts$/,
           use: [
